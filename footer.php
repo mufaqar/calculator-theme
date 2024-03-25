e680f806cd56bba0e35a563f5072fa3ae865f433
Mufaqar Islam <mufaqar@gmail.com>
2024-03-25 17:36:15 +0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,8 +1,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-
-
 <?php wp_footer(); ?>
 
 <script>
